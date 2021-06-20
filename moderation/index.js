4b7d2327d47b7f56fd1b3c5b38ca2ab7b37ba6e7
Miki Masumoto <masumotomiki18@gmail.com>
2021-06-20 09:41:33 +0900

    WIP pending comments bug

---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.post("/events", async (req, res) => {
+    console.log('Received event in moderation :>> ', req.body.type);
+    const { type, data } = req.body;
+
+    if (type === "CommentCreated") {
+        const status = data.content.includes("orange") ? "rejected" : "approved";
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentModerated",
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+    res.send({});
+});
+
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
