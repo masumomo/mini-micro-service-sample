5e2e0d28f42dc0371c6bec42a6f2c2ed205efd14
Miki Masumoto <masumotomiki18@gmail.com>
2021-06-22 08:37:29 +0900

    Add k8s yml files

--- comments/index.js
+++ comments/index.js
@@ -23,8 +23,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[postId] = comments;
 
-  console.log('postId :>> ', postId);
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated", data: { id: commentId, content, postId, status: "pending" }
   })
     .catch((err) => {
@@ -44,7 +43,7 @@ app.post("/events", async (req, res) => {
     comment.status = status;
     comment.content = content;
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated", data: { id, content, postId, status }
     })
       .catch((err) => {
